27373485bf45515f66e3ac9985a3161dc5c7b8ff
TheKhoshhal <thekhoshhal@gmail.com>
2024-06-17 16:57:58 +0330

    setup needed configs

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,8 +3,12 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local bufnr = vim.api.nvim_get_current_buf()
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<leader>a", function()
+  vim.cmd.RustLsp "codeAction"
+end, { silent = true, buffer = bufnr })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
