a5641dd60ba720f3584cadc82509c6bf69bb2cef
TheKhoshhal <thekhoshhal@gmail.com>
2024-06-22 20:22:32 +0330

    go to definition key binding

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,8 +7,20 @@ local bufnr = vim.api.nvim_get_current_buf()
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+-- map("n", "<leader>ca", function()
+--   vim.cmd.RustLsp "codeAction"
+-- end, { silent = true, buffer = bufnr })
+
 map("n", "<leader>ca", function()
-  vim.cmd.RustLsp "codeAction"
-end, { silent = true, buffer = bufnr })
+  vim.lsp.buf.code_action()
+end, { desc = "Code Actions" })
+
+map("n", "<leader>gd", function()
+  vim.lsp.buf.definition()
+end, { desc = "go to defintion" })
+
+map("n", "<leader>rn", function()
+  vim.lsp.buf.rename()
+end, { desc = "rename" })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
