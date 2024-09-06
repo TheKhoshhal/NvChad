5b7ad2b474f8bdd895b61813e555194107a857f9
TheKhoshhal <thekhoshhal@gmail.com>
2024-09-07 01:34:42 +0330

    removed autosave

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "cssls", "rust_analyzer", "gopls", "pyright", "bashls", "yamlls" }
+local servers = { "cssls", "rust_analyzer", "gopls", "pyright", "bashls", "yamlls", "sqlls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -22,7 +22,6 @@ lspconfig.tsserver.setup {
   capabilities = capabilities,
 }
 
-lspconfig.gleam.setup({})
 
 -- lspconfig.html.setup({
 --     on_attach = on_attach,
