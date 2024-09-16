237d72d7a318ec496c910fc120ffc366a8dcc21b
TheKhoshhal <thekhoshhal@gmail.com>
2024-09-16 13:54:56 +0330

    added rust-analyzer again

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,10 +1,10 @@
--- EXAMPLE
+-- EXAMPLE 
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "cssls", "gopls", "pyright", "bashls", "yamlls", "sqlls" }
+local servers = { "cssls", "rust_analyzer", "gopls", "pyright", "bashls", "yamlls", "sqlls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -15,21 +15,22 @@ for _, lsp in ipairs(servers) do
   }
 end
 
-lspconfig.html.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "html", "templ" },
-}
 
-lspconfig.tailwindcss.setup {
-  on_attach = on_attach,
-  capabilities = capabilities,
-  filetypes = { "templ", "astro", "javascript", "typescript", "react" },
-  settings = {
-    tailwindCSS = {
-      includeLanguages = {
-        templ = "html",
+lspconfig.html.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "html", "templ" },
+})
+
+lspconfig.tailwindcss.setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+    filetypes = { "templ", "astro", "javascript", "typescript", "react" },
+    settings = {
+      tailwindCSS = {
+        includeLanguages = {
+          templ = "html",
+        },
       },
     },
-  },
-}
+})
